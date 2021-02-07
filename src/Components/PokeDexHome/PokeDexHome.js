28fe781c05fbbaba83be2d0219517bfdae86b9c7
Programmer Nazmul <58470993+pronazmul@users.noreply.github.com>
2021-02-07 21:07:53 +0600

    Appbar Included

--- src/Components/PokeDexHome/PokeDexHome.js
+++ src/Components/PokeDexHome/PokeDexHome.js
@@ -1,6 +1,23 @@
+import { Grid } from "@material-ui/core";
+import PokemonAppbar from "./PokemonAppbar";
+
 const PokeDexHome = (props) => {
-  console.log(props);
-  return "this is pokeDex home Page";
+  return (
+    <Grid container>
+      {/* Main Header */}
+      <Grid item xs={12}>
+        <PokemonAppbar />
+      </Grid>
+      {/* Main Card Component */}
+      <Grid item container>
+        <Grid item xs={false} sm={2}></Grid>
+        <Grid item container xs={12} sm={8}>
+          {" "}
+        </Grid>
+        <Grid item xs={false} sm={2}></Grid>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default PokeDexHome;
